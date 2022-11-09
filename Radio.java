925f87864c85d979b0617698c98b262a3c236241
Jose-Prince <joseprince148mail.com>
2022-11-08 21:22:44 -0600

    Modificación Radio
    Se completo la clase Radio

--- Radio.java
+++ Radio.java
@@ -1,3 +1,41 @@
 public class Radio {
-    
+    private String banda;
+    private int volumen;
+    private boolean encendido;
+
+    public Radio(){
+        banda = "";
+        volumen = 0;
+        encendido = false;
+    }
+
+
+    public String getBanda() {
+        return this.banda;
+    }
+
+    public void setBanda(String banda) {
+        this.banda = banda;
+    }
+
+    public int getVolumen() {
+        return this.volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    public boolean isEncendido() {
+        return this.encendido;
+    }
+
+    public boolean getEncendido() {
+        return this.encendido;
+    }
+
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+
 }
